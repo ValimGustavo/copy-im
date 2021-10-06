48ebe1ba7c5bd860ca101b491983a4fc7d58ea33
ValimGustavo <gustavo.souza.valim@gmail.com>
2021-10-06 09:52:25 -0300

    feat: add sanatization to bash language

--- content.js
+++ content.js
@@ -1,21 +1,16 @@
 ﻿setInterval(() => {
-  let id = 11111111111111111111111;
   posts = document.querySelectorAll('div[class="post-code"]');
   posts.forEach((elem) => {
     if (!elem.querySelector('div[data-copy="true"]')) {
-      const div = document.createElement("div");
-      div.style.width = "40px";
-      div.style.height = "15px";
-      div.style.background = "red";
-      id++;
-      div.setAttribute("id", id);
-      div.style.backgroundColor = "red";
-      div.style.zIndex = 100;
-      div.setAttribute("data-copy", "true");
-      
+      const div = divCreate();
+
       div.addEventListener("click", (event) => {
+        
+        let content = elem.querySelector('div[class="hljs"] > code').textContent 
+        let language = elem.querySelector('span[class="post-code__language"]').textContent.trim().toLowerCase()
+        
         navigator.clipboard.writeText(
-          elem.querySelector('div[class="hljs"] > code').textContent
+          sanatization(content, language)
         );
       });
 
@@ -23,3 +18,32 @@
     }
   });
 }, 1000);
+
+function divCreate() {
+  const div = document.createElement("div");
+  div.style.width = "40px";
+  div.style.height = "15px";
+  div.style.background = "red";
+  div.setAttribute("id", Date.now());
+  div.style.backgroundColor = "red";
+  div.style.zIndex = 100;
+  div.setAttribute("data-copy", "true");
+
+  return div;
+}
+
+
+function sanatization(content, language){
+  switch(language){
+    case 'bash':{
+      if(content.indexOf('$') == 0){
+        return content.replace('$', '');
+      }
+    }
+
+    default: {
+      return content;
+      break;
+    }
+  }
+}
